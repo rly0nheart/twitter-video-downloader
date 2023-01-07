2d02b56ad40dcc80ab667e3f15a37296fa18b390
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-01-07 04:16:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setuptools.setup(
     name='twitter-video-downloader',
-    version='2023.1.0.0',
+    version='1.0.0',
     author='Richard Mwewa',
     author_email='rly0nheart@duck.com',
     packages=['twitter_video_downloader'],
